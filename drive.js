2a5a96acc8baa00a5930186b56c9b9032f00efb1
長崎克秀 <katsu3@nagasakikatsuhidenoMacBook-Air.local>
2025-11-25 04:17:13 +0900

    不要なファイル削除

--- drive.js
+++
@@ -1,7 +0,0 @@
-async function listDriveFiles(folderId) {
-  const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&fields=files(id,name,webContentLink,thumbnailLink,webViewLink)`;
-
-  const res = await fetch(url);
-  const json = await res.json();
-  return json.files || [];
-}
